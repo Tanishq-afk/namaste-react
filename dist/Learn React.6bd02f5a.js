e4694dfc6251c1f8d04924fa27f25e1676980c9a
Tanishq Sachdev <tanishqsachdev16@gmail.com>
2026-03-27 13:22:42 +0530

    real episode-01

---
+++ dist/Learn React.6bd02f5a.js
@@ -0,0 +1,5 @@
+const heading = React.createElement('h1', {}, 'Hello World from React!');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+//# sourceMappingURL=Learn React.6bd02f5a.js.map
